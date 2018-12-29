c65ac581a8256e495ed1f19000f6fceb57708cd5
Thangameenahamsan <thangameena2462000@gmail.com>
2018-12-29 16:08:47 +0530

    first commit

---
+++ postfix.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<math.h>
+#define MAX 50
+int stack[MAX];
+char post[MAX];
+int top=1;
+void pushstack(int temp);
+void calculator(char c);
+void main()
+{
+int i;
+clrscr();
+printf("Enter the postfix notation:");
+gets(post);
+for(i=0;i<strlen(post);i++)
+{
+if(post[i]>='0'&&post[i]<='9')
+pushstack(i);
+if(post[i]=='+'||post[i]=='-'||post[i]=='*'||post[i]=='/'||post[i]=='^')
+calculator(post[i]);
+}
+printf("\n Result:%d",stack[top]);
+getch();
+}
+void pushstack(int temp)
+{
+top++;
+stack[top]=(int)(post[temp]-48);
+}
+void calculator(char c)
+{
+int a,b,ans;
+a=stack[top];
+stack[top]='\0';
+top--;
+b=stack[top];
+stack[top]='\0';
+stack[top];
+top--;
+switch(c)
+{
+case'+':ans=b+a;break;
+case'-':ans=b-a;break;
+case'*':ans=b*a;break;
+case'/':ans=b/a;break;
+case'^':ans=pow(b,a);break;
+default:ans=0;
+}
+top++;
+stack[top]=ans;
+}
